538253d6826a45988f693280d251e72d24f56ea8
Manya Kalra <kalramanya212@gmail.com>
2025-09-23 17:07:19 +0530

    Fix typo

--- SnakeGame.java
+++ SnakeGame.java
@@ -172,3 +172,4 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
 <!-- Update 2025-05-27T11:57:56+05:30 -->
 <!-- Update 2025-06-20T16:57:00+05:30 -->
 <!-- Update 2025-07-22T08:35:07+05:30 -->
+<!-- Update 2025-09-23T17:07:19+05:30 -->
